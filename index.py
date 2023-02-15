9814fb82ddeafbea7fec175b55f6e5ed77687b72
brunounky <bruno_unky@hotmail.com>
2023-02-15 20:13:38 -0300

    Initial commit

--- index.py
+++ index.py
@@ -1,3 +1,30 @@
 print('Primeiro Comando')
 print('Segundo Comando')
 print('Terceiro Comando')
+
+### Seleção
+
+condicao = False
+
+if condicao:
+    print('passou por aqui 1')
+condicao = True
+if condicao:
+    print('passou por aqui 2')
+
+###Iteração
+
+lista = list(range(1,10))
+for numero in lista:
+    print(numero)
+
+###Função
+
+def caixinha_magica(numero1, numero2):
+    numero_magico = numero1 + numero2
+    return numero_magico
+
+print(caixinha_magica(2,5))
+
+###
+
