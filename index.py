0d9b2e066a6b7f770cc9333510c11ded3eb2664e
brunounky <bruno_unky@hotmail.com>
2023-02-15 20:36:46 -0300

    Initial commit

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ print('Primeiro Comando')
 print('Segundo Comando')
 print('Terceiro Comando')
 
+print('__________________________________')
+
 ### Seleção
 
 condicao = False
@@ -12,11 +14,15 @@ condicao = True
 if condicao:
     print('passou por aqui 2')
 
+print('__________________________________')
+
 ###Iteração
 
 lista = list(range(1,10))
 for numero in lista:
     print(numero)
+
+print('__________________________________')
 
 ###Função
 
@@ -26,5 +32,23 @@ def caixinha_magica(numero1, numero2):
 
 print(caixinha_magica(2,5))
 
-###
 
+print('__________________________________')
+
+###Teste
+
+assert caixinha_magica(2,2) == 4
+### Permite que seja dado os parâmetros que serão utilizados e a resposta que ele deveria ter. Assim é introduzido o teste unitário.
+### Teste unitário é mensurar um desenvolvimento
+
+print('__________________________________')
+
+print('Primeiro experimento com TDD')
+
+def caixinha_magica2(parametro1, paramentro2):
+    retorno_magico = parametro1 -paramentro2
+    return retorno_magico
+
+assert caixinha_magica2(10, 4) == 6
+assert caixinha_magica2(10, 5) == 5
+### Depois disso pode ser feito a refatoração do código, ou seja, melhorar a forma como o mesmo está estruturado
